8b37e576634bc7706b735f5fc3c9fb7cbe01223f
ayvengoza <ayvengoza@gmail.com>
2017-11-19 21:34:17 +0200

    add menu

--- app/src/main/java/com/ayvengoza/locatr/LocaltrFragment.java
+++ app/src/main/java/com/ayvengoza/locatr/LocaltrFragment.java
@@ -4,14 +4,19 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Created by ayven on 19.11.2017.
  */
 
 public class LocaltrFragment extends Fragment {
+    private ImageView mImageView;
+
     public static LocaltrFragment newInstance(){
         Bundle args = new Bundle();
         LocaltrFragment fragment = new LocaltrFragment();
@@ -19,10 +24,23 @@ public class LocaltrFragment extends Fragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_locatr, container, false);
+        mImageView = (ImageView) view.findViewById(R.id.image);
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
     }
 }
